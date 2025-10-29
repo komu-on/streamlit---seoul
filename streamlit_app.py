0cd5b2bce3cada4f18b2ec4d3d4d260c63d4db2e
komu-on <ony0908@naver.com>
2025-10-29 14:22:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("Hello Streamlit-er 👋")
 st.markdown(
@@ -15,3 +17,21 @@ st.markdown(
 
 if st.button("Send balloons!"):
     st.balloons()
+
+
+st.write("Steamlit supports a wide range of data visualizations")
+
+all_users=['Alice','Bob','Charly']
+with st.container(border=True):
+    users=st.multiselect('Users',all_users, default=all_users)
+    rolling_average=st.toggle("Rolling average")
+
+np.random.seed(42)
+data=pd.DataFrame(np.random.randn(20, len(users)),columns=users)
+
+if rolling_average:
+    data=data.rolling(7).mean().dropna()
+
+tab1, tab2=st.tabs(["Chart", "Dataframe"])
+tab1.line_chart(data, height=250)
+tab2.dataframe(data, height=250, use_container_width=True)
